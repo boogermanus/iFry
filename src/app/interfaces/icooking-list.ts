654b091e93ef955b042736c021c7e4b60fb4b4da
Jordan Woodruff <boogermanus@gmail.com>
2025-03-27 16:19:17 -0500

    interfaces and starting on data

---
+++ src/app/interfaces/icooking-list.ts
@@ -0,0 +1,5 @@
+import { ICooking } from "./icooking";
+
+export interface ICookingList {
+    list: ICooking[];
+}
---
+++ src/app/interfaces/icooking.ts
@@ -0,0 +1,9 @@
+export interface ICooking {
+    category: string;
+    ingredient: string;
+    amount: string;
+    preparation: string;
+    oil: string;
+    tempteure: number; // in F
+    cookTime: string;
+}
