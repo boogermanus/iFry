b73239c5482718df58ce927649caef0c5227b51b
Jordan Woodruff <boogermanus@gmail.com>
2025-06-28 21:34:18 -0500

    store initial commit

---
+++ src/app/stores/IfryStore.ts
@@ -0,0 +1,14 @@
+﻿import {signalStore, withState} from "@ngrx/signals";
+
+type IFryType = {
+  temp: number;
+}
+
+export const IFryInitialState: IFryType = {
+  temp: 0,
+}
+
+export const IFryStore = signalStore(
+  withState<IFryType>(IFryInitialState)
+)
+
