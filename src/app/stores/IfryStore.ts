682463fb2f54735fea87c222d9f2d142f0e2e56a
Jordan Woodruff <boogermanus@gmail.com>
2025-06-30 12:36:23 -0500

    more store methods

--- src/app/stores/IfryStore.ts
+++ src/app/stores/IfryStore.ts
@@ -1,14 +1,27 @@
-﻿import {signalStore, withState} from "@ngrx/signals";
+﻿import {patchState, signalStore, withMethods, withState} from "@ngrx/signals";
+import {IfryService} from "../services/ifry.service";
+import {inject} from "@angular/core";
 
 type IFryType = {
   temp: number;
+  time: number;
 }
 
 export const IFryInitialState: IFryType = {
   temp: 0,
+  time: 0
 }
 
 export const IFryStore = signalStore(
-  withState<IFryType>(IFryInitialState)
-)
+  withState<IFryType>(IFryInitialState),
+  withMethods((store, service = inject(IfryService)) => ({
+    update(temp: number, time: number): void {
+      let result = service.calculate(temp, time);
+
+      if(result.Temp > 0 && result.Time > 0) {
+        patchState(store, {temp: result.Temp, time: result.Time});
+      }
+    }
+  }))
+);
 
