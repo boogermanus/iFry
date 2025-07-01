97e362bfe86b691ce28ac215c83dc1c03d622947
Jordan Woodruff <boogermanus@gmail.com>
2025-07-01 14:04:01 -0500

    remove unused code

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -7,9 +7,6 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {CookingPipe} from 'src/app/pipes/cooking.pipe';
 import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-
 @Component({
   selector: 'app-recipes',
   imports: [
